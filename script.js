ad1767e28b6737b53bea3a2d543b4427066e49cd
Jackson-Naufal <>
2021-04-04 15:43:44 +0000

    reverted back

--- script.js
+++ script.js
@@ -6,131 +6,131 @@ document.getElementById('button').addEventListener('click', button)
 function button () {
   date = document.getElementById('input').value
   parseInt(date)
- if(date === 51 || date === 1970)
+ if(date == 51 || date == 1970)
  {
    document.getElementById('para').innerHTML = (' Test ')
  }
- else if ((date === 50) || (date === 1971)) {
+ else if ((date == 50) || (date == 1971)) {
    document.getElementById('para').innerHTML = (' Test2.0 ')
 
 
- } else if ((date === 49) || (date === 1972)) {
+ } else if ((date == 49) || (date == 1972)) {
    document.getElementById('para').innerHTML = (' Test3.0 ')
 
 
- } else if ((date === 48) || (date === 1973)) {
+ } else if ((date == 48) || (date == 1973)) {
    document.getElementById('para').innerHTML = (' Test4.0 ')
 
 
- } else if ((date === 47) || (date === 1974)) {
+ } else if ((date == 47) || (date == 1974)) {
    document.getElementById('para').innerHTML = (' Test5.0 ')
 
 
- } else if ((date === 46) || (date === 1975)) {
+ } else if ((date == 46) || (date == 1975)) {
    document.getElementById('para').innerHTML = (' Test6.0 ')
 
 
- } else if ((date === 45) || (date === 1976)) {
+ } else if ((date == 45) || (date == 1976)) {
    document.getElementById('para').innerHTML = (' Test7.0 ')
 
 
- } else if ((date === 44) || (date === 1977)) {
+ } else if ((date == 44) || (date == 1977)) {
    document.getElementById('para').innerHTML = (' Test8.0 ')
 
 
- } else if ((date === 43) || (date === 1978)) {
+ } else if ((date == 43) || (date == 1978)) {
    document.getElementById('para').innerHTML = (' Test9.0 ')
 
 
- } else if ((date === 42) || (date === 1979)) {
+ } else if ((date == 42) || (date == 1979)) {
    document.getElementById('para').innerHTML = (' Test10.0 ')
 
 
- } else if ((date === 41) || (date === 1980)) {
+ } else if ((date == 41) || (date == 1980)) {
    document.getElementById('para').innerHTML = (' Test11.0 ')
 
 
- } else if ((date === 40) || (date === 1981)) {
+ } else if ((date == 40) || (date == 1981)) {
    document.getElementById('para').innerHTML = (' Test12.0 ')
 
 
- } else if ((date === 39) || (date === 1982)) {
+ } else if ((date == 39) || (date == 1982)) {
    document.getElementById('para').innerHTML = (' Test13.0 ')
 
    
- } else if ((date === 38) || (date === 1983)) {
+ } else if ((date == 38) || (date == 1983)) {
    document.getElementById('para').innerHTML = (' Test14.0 ')
 
    
- } else if ((date === 37) || (date === 1984)) {
+ } else if ((date == 37) || (date == 1984)) {
    document.getElementById('para').innerHTML = (' Test15.0 ')
 
 
- } else if ((date === 36) || (date === 1985)) {
+ } else if ((date == 36) || (date == 1985)) {
    document.getElementById('para').innerHTML = (' Test16.0 ')
 
 
- } else if ((date === 35) || (date === 1986)) {
+ } else if ((date == 35) || (date == 1986)) {
    document.getElementById('para').innerHTML = (' Test17.0 ')
 
    
- } else if ((date === 34) || (date === 1987)) {
+ } else if ((date == 34) || (date == 1987)) {
    document.getElementById('para').innerHTML = (' Test18.0 ')
 
 
- } else if ((date === 33) || (date === 1988)) {
+ } else if ((date == 33) || (date == 1988)) {
    document.getElementById('para').innerHTML = (' Test19.0 ')
 
    
- } else if ((date === 32) || (date === 1989)) {
+ } else if ((date == 32) || (date == 1989)) {
    document.getElementById('para').innerHTML = (' Test20.0 ')
 
 
- } else if ((date === 31) || (date === 1990)) {
+ } else if ((date == 31) || (date == 1990)) {
    document.getElementById('para').innerHTML = (' Test21.0 ')
 
    
- } else if ((date === 30) || (date === 1991)) {
+ } else if ((date == 30) || (date == 1991)) {
    document.getElementById('para').innerHTML = (' Test22.0 ')
 
 
- } else if ((date === 29) || (date === 1992)) {
+ } else if ((date == 29) || (date == 1992)) {
    document.getElementById('para').innerHTML = (' Test23.0 ')
 
 
- } else if ((date === 28) || (date === 1993)) {
+ } else if ((date == 28) || (date == 1993)) {
    document.getElementById('para').innerHTML = (' Test24.0 ')
 
 
- } else if ((date === 27) || (date === 1994)) {
+ } else if ((date == 27) || (date == 1994)) {
    document.getElementById('para').innerHTML = (' Test25.0 ')
 
 
- } else if ((date === 26) || (date === 1995)) {
+ } else if ((date == 26) || (date == 1995)) {
    document.getElementById('para').innerHTML = (' Test26.0 ')
 
 
- } else if ((date === 25) || (date === 1996)) {
+ } else if ((date == 25) || (date == 1996)) {
    document.getElementById('para').innerHTML = (' Test27.0 ')
 
 
- } else if ((date === 24) || (date === 1997)) {
+ } else if ((date == 24) || (date == 1997)) {
    document.getElementById('para').innerHTML = (' Test28.0 ')
 
 
- } else if ((date === 23) || (date === 1998)) {
+ } else if ((date == 23) || (date == 1998)) {
    document.getElementById('para').innerHTML = (' Test29.0 ')
 
 
- } else if ((date === 22) || (date === 1999)) {
+ } else if ((date == 22) || (date == 1999)) {
    document.getElementById('para').innerHTML = (' Test30.0 ')
 
 
- } else if ((date === 21) || (date === 2000)) {
+ } else if ((date == 21) || (date == 2000)) {
    document.getElementById('para').innerHTML = (' Test31.0 ')
 
 
- } else if ((date === 20) || (date === 2001)) {
+ } else if ((date == 20) || (date == 2001)) {
    document.getElementById('para').innerHTML = (' Test32.0 ')
  }
 }
