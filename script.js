6ad60b977b5826a3fe16fa5443434ff04d627bc1
Jackson-Naufal <>
2021-04-05 14:36:23 +0000

    Spaces

--- script.js
+++ script.js
@@ -5,16 +5,13 @@ document.getElementById('button').addEventListener('click', button)
 function button () {
   date = document.getElementById('input').value
   parseInt(date)
- if (date >= 51)
- {
+  if (date >= 51) {
    document.getElementById('good').innerHTML = (' in 1970, the famous apollo 13 mission was ago, it was April 11th, 1970, when the spaceship was first launched, and on April 17th, 1970, is when it first made contact on the moon, making it one of the most significant breakthroughs in space travel! ')
-  document.getElementById('bad').innerHTML = (' One bad thing that happened in 1970, was that President, Richard Nixon, ordered and invasion in Cambodia, which made the Vietnam war, significantly worse. ')
-  }
-  else if (date >= 50) 
-  {
-   document.getElementById('good').innerHTML = (' good2 ')
+   document.getElementById('bad').innerHTML = (' One bad thing that happened in 1970, was that President, Richard Nixon, ordered and invasion in Cambodia, which made the Vietnam war, significantly worse. ')
+    }
+  else if (date >= 50) {
+   document.getElementById('good').innerHTML = (' good2')
   document.getElementById('bad').innerHTML = (' bad2 ')
-
 
  } else if (date >= 49)  {
    document.getElementById('good').innerHTML = (' good3 ')
