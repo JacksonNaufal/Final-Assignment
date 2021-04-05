68a6097584167c0963e82ddd6d56edd1349047eb
Jackson-Naufal <>
2021-04-05 15:29:51 +0000

    Added more ages

--- script.js
+++ script.js
@@ -13,14 +13,34 @@ function button () {
   } else if (date >= 50) {
     document.getElementById('good').innerHTML = ('This year was known to be one of the years that started the digital age due to the reason that the microprocessor was first invented.')
     document.getElementById('bad').innerHTML = ('This is not a bad fact, but this was the year that a bad man, who was a famous serial killer, Charles Manson, was arrested, and where he was put in jail.')
-
   } else if (date >= 49) {
-    document.getElementById('good').innerHTML = ('good3')
+    document.getElementById('good').innerHTML = ('There was a New Space Shuttle program that was released to the general public, which would help humans take leaps in the space industry.')
     document.getElementById('bad').innerHTML = ('This year had many significant earthquakes in Nicaragua, Turkey, Iraq, and many other tragedies like terrorist attacks in Isreal!')
-
   } else if (date >= 48) {
-    document.getElementById('good').innerHTML = ('good4')
-    document.getElementById('bad').innerHTML = ('bad4')
+    document.getElementById('good').innerHTML = ('The Famous Skylab is launched, and also the sears tower is done construction, and this is also the year that President Richard Nixon starts his second term as president.')
+    document.getElementById('bad').innerHTML = ('Spiro Agnew, who was vice president of America, resigned from his Vice President, Roll.')
+  } else if (date >= 47) {
+    document.getElementById('good').innerHTML = ('good5')
+    document.getElementById('bad').innerHTML = ('bad5')
+  }
+  } else if (date >= 46) {
+    document.getElementById('good').innerHTML = ('good5')
+    document.getElementById('bad').innerHTML = ('bad5')
+  }
+  } else if (date >= 45) {
+    document.getElementById('good').innerHTML = ('good5')
+    document.getElementById('bad').innerHTML = ('bad5')
+  }
+  } else if (date >= 44) {
+    document.getElementById('good').innerHTML = ('good5')
+    document.getElementById('bad').innerHTML = ('bad5')
+  }
+  } else if (date >= 43) {
+    document.getElementById('good').innerHTML = ('good5')
+    document.getElementById('bad').innerHTML = ('bad5')
+  }
+  } else if (date >= 42) {
+    document.getElementById('good').innerHTML = ('good5')
+    document.getElementById('bad').innerHTML = ('bad5')
   }
 }
-
