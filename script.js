8bcc7a6e1183d93e781f05f3186cf0a53a2ee937
Jackson-Naufal <>
2021-04-06 15:06:55 +0000

    Format

--- script.js
+++ script.js
@@ -107,12 +107,12 @@ function button () {
   }
 }
 
-  function button2 () {
-   age = document.getElementById('input2').value
-   age = parseInt(age)
-   number = year - age
-   document.getElementById('def').innerHTML =
-   ('Were you born in')
+function button2 () {
+  age = document.getElementById('input2').value
+  age = parseInt(age)
+  number = year - age
+  document.getElementById('def').innerHTML =
+  ('Were you born in')
    document.getElementById('guesser').innerHTML =
    number
- }
+}
