ffc96ae06a5f17b0ff85f2be81ffb5a756c9c770
Jackson-Naufal <>
2021-04-06 16:49:07 +0000

    Format

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const min = 21
 document.getElementById('button').addEventListener('click', button)
 document.getElementById('button2').addEventListener('click', button2)
 
-while (guess > birth || guess < min ) {
+while(guess > birth || guess < min ) {
   guess = prompt('Enter your age!')
   guess = parseInt(guess)
   if (guess < min) {
-  alert('You are to young for this programs only ages between the ages 21 and 51 are elligible!') 
+    alert('You are to young for this programs only ages between the ages 21 and 51 are elligible!')
   } else if (guess > birth) {
     alert('You are to old for this program, only ages between 21 and 51 are elligible.')
   } else if (guess < birth || guess < min) {
